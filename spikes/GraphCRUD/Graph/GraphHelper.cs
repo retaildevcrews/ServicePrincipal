1d8caf9ad32ba5b21c4dc6d40484935217a10188
Joaquin Rodriguez <rjoaquin@microsoft.com>
2020-08-11 09:27:27 -0500

    Added Graph CRUD spike

---
+++ spikes/GraphCRUD/Graph/GraphHelper.cs
@@ -0,0 +1,123 @@
+using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace GraphCrud
+{
+    public class GraphHelper
+    {
+        private static GraphServiceClient graphClient;
+
+        private static string deltaLinkValue;
+
+        public static void Initialize(IAuthenticationProvider authProvider)
+        {
+            graphClient = new GraphServiceClient(authProvider);
+        }
+
+        public static async Task<User> GetMeAsync()
+        {
+            try
+            {
+                // GET /me
+                return await graphClient.Me.Request().GetAsync();
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting signed-in user: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IEnumerable<User>> GetAllUsersAsync()
+        {
+            try
+            {
+                var resultPage = await graphClient.Users.Request()
+                // Only return the fields used by the application
+                .Select(e => new
+                {
+                    e.DisplayName,
+                })
+
+                .GetAsync();
+                return resultPage.CurrentPage;
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error getting All Users: {ex.Message}");
+                return null;
+            }
+        }
+
+        public static async Task<IEnumerable<User>> GetUsersDeltaAsync()
+        {
+            IUserDeltaCollectionPage userCollectionPage;
+
+            var userList = new List<User>();
+
+            if (String.IsNullOrWhiteSpace(deltaLinkValue))
+            {
+                Console.WriteLine("No deltaLink found. Initializing...");
+                userCollectionPage = await graphClient.Users.Delta().Request().GetAsync(); ;
+            }
+            else
+            {
+                userCollectionPage = new UserDeltaCollectionPage();
+                userCollectionPage.InitializeNextPageRequest(graphClient, deltaLinkValue);
+                userCollectionPage = await userCollectionPage.NextPageRequest.GetAsync();
+            }
+
+            // Populate result
+            userList.AddRange(userCollectionPage.CurrentPage);
+
+            while (userCollectionPage.NextPageRequest != null)
+            {
+	            userCollectionPage = await userCollectionPage.NextPageRequest.GetAsync();
+	            userList.AddRange(userCollectionPage.CurrentPage);
+            }
+
+
+            if (userCollectionPage.AdditionalData.TryGetValue("@odata.deltaLink", out object deltaLink))
+            {
+                deltaLinkValue = deltaLink.ToString();
+            }
+
+            return userList;
+
+        }
+
+
+        public static async void createUser(string firstName, string lastName)
+        {
+            var displayName = firstName + " " + lastName;
+
+            PasswordProfile p = new PasswordProfile();
+            p.Password = "giPA14PKm&3k%PoJ";
+
+            var user = new User
+            {
+                AccountEnabled = true,
+                DisplayName = displayName,
+                MailNickname = firstName + lastName,
+                GivenName = firstName,
+                JobTitle = "Software Engineer",
+                PreferredLanguage = "en-US",
+                UserPrincipalName = firstName.Substring(0, 3) + lastName + "@rdzv.onmicrosoft.com",
+                PasswordProfile = p
+            };
+
+            try
+            {
+                var newUser = await graphClient.Users
+                    .Request()
+                    .AddAsync(user);
+            }
+            catch (ServiceException ex)
+            {
+                Console.WriteLine($"Error creating User {user.UserPrincipalName} : {ex.Message}");
+            }
+        }
+    }
+}
---
+++ spikes/GraphCRUD/Program.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+
+namespace GraphCrud
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Graph API Sample CRUD App \n");
+
+            var appConfig = LoadAppSettings();
+
+            if (appConfig == null)
+            {
+                Console.WriteLine("Missing or invalid appsettings.json...exiting");
+                return;
+            }
+
+            var appId = appConfig["appId"];
+            var scopesString = appConfig["scopes"];
+            var scopes = scopesString.Split(';');
+
+            // Initialize the auth provider with values from appsettings.json
+            var authProvider = new DeviceCodeAuthProvider(appId, scopes);
+
+            // Request a token to sign in the user
+            var accessToken = authProvider.GetAccessToken().Result;
+
+            // Initialize Graph client
+            GraphHelper.Initialize(authProvider);
+
+            // Get signed in user
+            var user = GraphHelper.GetMeAsync().Result;
+            Console.WriteLine($"Welcome {user.DisplayName}!\n");
+
+            int choice = -1;
+
+            while (choice != 0)
+            {
+                Console.WriteLine("Please choose one of the following options:");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine("1. Display access token");
+                Console.WriteLine("2. List All Users");
+                Console.WriteLine("3. Add New User");
+                Console.WriteLine("4. List Users (Delta)");
+
+                try
+                {
+                    choice = int.Parse(Console.ReadLine());
+                }
+                catch (System.FormatException)
+                {
+                    // Set to invalid value
+                    choice = -1;
+                }
+
+                switch (choice)
+                {
+                    case 0:
+                        Console.WriteLine("Goodbye...");
+                        break;
+                    case 1:
+                        Console.WriteLine($"Access token: {accessToken}\n");
+                        break;
+                    case 2:
+                        ListAllUsers();
+                        break;
+                    case 3:
+                        createNewUser();
+                        break;
+                    case 4:
+                        ListUsersDelta();
+                        break;    
+                    default:
+                        Console.WriteLine("Invalid choice! Please try again.");
+                        break;
+                }
+            }
+        }
+
+        static IConfigurationRoot LoadAppSettings()
+        {
+            var appConfig = new ConfigurationBuilder()
+                .AddUserSecrets<Program>()
+                .Build();
+
+            // Check for required settings
+            if (string.IsNullOrEmpty(appConfig["appId"]) ||
+                string.IsNullOrEmpty(appConfig["scopes"]))
+            {
+                return null;
+            }
+            return appConfig;
+        }
+
+        static void ListAllUsers()
+        {
+            var users = GraphHelper.GetAllUsersAsync().Result;
+        
+            foreach (var user in users)
+            {
+                Console.WriteLine($"User: {user.DisplayName}");
+            }
+        }
+
+        static void ListUsersDelta()
+        {
+            var users = GraphHelper.GetUsersDeltaAsync().Result;
+            
+            foreach (var user in users)
+            {
+                Console.WriteLine(user.DisplayName + ":" + user.Id);
+            }
+        }
+
+
+        static void createNewUser()
+        {
+            Console.WriteLine("Enter User First Name:");
+            string firstName = Console.ReadLine();
+            Console.WriteLine("Enter User Last Name:");
+            string lastName = Console.ReadLine();
+            GraphHelper.createUser(firstName, lastName);
+        }
+    }
+}
