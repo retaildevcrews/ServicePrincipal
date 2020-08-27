4435b5fe710ae4f4e7d64fb7a2dd277d2bf9617f
Joaquin Rodriguez <rjoaquin@microsoft.com>
2020-08-27 09:49:45 -0500

    Scaffolding

---
+++ src/Automation/CSE.Automation/GraphDeltaProcessor.cs
@@ -0,0 +1,41 @@
+using System.Configuration;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+namespace CSE.Automation
+{
+    public static class GraphDeltaProcessor
+    {
+        private static string clientId = ConfigurationManager.AppSettings.Get("clientId");
+        private static string tenantId = ConfigurationManager.AppSettings.Get("tenantId");
+        private static string clientSecret = ConfigurationManager.AppSettings.Get("clientSecret");
+
+        [FunctionName("ServicePrincipalDeltas")]
+        public static void Run([TimerTrigger("0 */2 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            if(string.IsNullOrEmpty(clientId) || string.IsNullOrEmpty(tenantId) || string.IsNullOrEmpty(clientSecret))
+            {
+                log.LogError("Error: Credentials Missing.");
+                    return;
+            }
+
+            IConfidentialClientApplication confidentialClientApplication = ConfidentialClientApplicationBuilder
+           .Create(clientId)
+           .WithTenantId(tenantId)
+           .WithClientSecret(clientSecret)
+           .Build();
+
+            ClientCredentialProvider authProvider = new ClientCredentialProvider(confidentialClientApplication);
+
+            // Initialize Graph client
+            //GraphHelper.Initialize(authProvider);
+
+
+            
+
+
+        }
+    }
+}
---
+++ src/Automation/CSE.Automation/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: FunctionsStartup(typeof(CSE.Automation.Startup))]
+
+namespace CSE.Automation
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+           
+            //builder.Services.AddSingleton<IMyService>((s) => {
+            //    return new MyService();
+            //});
+        }
+    }
+}
