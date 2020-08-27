1490fdc4f95fd6da0a16b9239f65a90c832b7688
Joaquin Rodriguez <rjoaquin@microsoft.com>
2020-08-27 14:24:28 -0500

    Added KeyVault scaffolding

---
+++ src/Automation/CSE.Automation/KeyVault/SecretService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security;
+using Azure.Security.KeyVault.Secrets;
+using CSE.Automation.Interfaces;
+
+namespace CSE.Automation.KeyVault 
+{
+    public class SecretService : ISecretClient
+    {
+        private SecretClient secretClient = default;
+        public SecretService(string keyVaultName)
+        {
+        }
+
+        public Uri Uri { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public KeyVaultSecret GetSecret(string secretName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SecureString GetSecretValue(string secretName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
