86b463f441e50ab884dd387cd6ab55d5d98cd638
Joseph Fultz <jofultz@hotmail.com>
2020-08-27 10:12:16 -0500

    Added some placeholder objects as examples

---
+++ src/Automation/CSE.Automation/Processors/ServicePrincipalProcessor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSE.Automation.Interfaces;
+
+namespace CSE.Automation.Processors
+{
+    public class ServicePrincipalProcessor:IDeltaProcessor
+    {
+        public void ProcessDeltas()
+        {
+
+        }
+    }
+}
