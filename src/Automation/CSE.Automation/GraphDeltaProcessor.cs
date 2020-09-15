20813d5dae05ef8d16c6677acc42990f74fe0d07
Joaquin Rodriguez <rjoaquin@microsoft.com>
2020-09-15 09:24:42 -0500

    Added Function for Seeding Service Principal Delta (#64)
    * Added Function for Seeding Service Principal Delta
    
    * Added some comments on pending items for seed function

--- src/Automation/CSE.Automation/GraphDeltaProcessor.cs
+++ src/Automation/CSE.Automation/GraphDeltaProcessor.cs
@@ -1,13 +1,18 @@
 using System;
-using System.Configuration;
+
 using System.Diagnostics;
 using System.Security;
 using CSE.Automation.Interfaces;
 using CSE.Automation.Utilities;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Microsoft.Graph.Auth;
-using Microsoft.Identity.Client;
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Graph;
+using System.Collections.Generic;
 
 namespace CSE.Automation
 {
@@ -44,5 +49,40 @@ namespace CSE.Automation
                 Debug.WriteLine(ex.Message);
             }
         }
+
+
+
+        [FunctionName("SeedServicePrincipal")]
+        public async Task<IActionResult> SeedServicePrincipal(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            try
+            {
+                var servicePrincipals = _graphHelper.SeedServicePrincipalDeltaAsync("appId,displayName,notes").Result;
+
+                foreach (var sp in servicePrincipals)
+
+                {
+                    if (String.IsNullOrWhiteSpace(sp.AppId) || String.IsNullOrWhiteSpace(sp.DisplayName))
+                        continue;
+                    //TODO
+                    //1. validate values
+                    //2. build ServicePrincipal POCO - depends on POCO definitions
+                    //3. save...
+                    //4. audit...
+                    log.LogInformation($"{sp.DisplayName} - {sp.AppId} - {sp.Notes}");
+                }
+            }
+            catch(Exception ex)
+            {
+                log.LogError(ex.Message);
+                Debug.WriteLine(ex.Message);
+            }
+           
+
+            return new OkObjectResult($"Success");
+        }
+
     }
 }
