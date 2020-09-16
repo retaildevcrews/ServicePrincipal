b2e4fa6eba914f021fd4ee014d92a1f75424e5eb
seanmiller168 <seanmiller168@gmail.com>
2020-09-16 10:28:13 -0700

    Sean 45 model notes (#74)
    * Created initial models for Graph Objects
    
    * Added models for data
    
    * Adding Name and Description fields to Configuration model
    
    Co-authored-by: Sean Miller <semill@microsoft.com>

---
+++ src/Automation/CSE.Automation/Interfaces/IGraphModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSE.Automation.Model
+{
+    public interface IGraphModel
+    {
+        public string Id { get; set; }
+
+        public DateTime Created { get; set; }
+
+        public DateTime Deleted { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public ObjectType ObjectType { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
---
+++ src/Automation/CSE.Automation/Model/ServicePrincipal.cs
@@ -0,0 +1,14 @@
+﻿
+namespace CSE.Automation.Model
+{
+    public class ServicePrincipal : GraphModel
+    {
+        public string AppId { get; set; }
+
+        public string AppDisplayName { get; set; }
+
+        public string DisplayName { get; set; }
+
+        public Notes Notes { get; set; }
+    }
+}
