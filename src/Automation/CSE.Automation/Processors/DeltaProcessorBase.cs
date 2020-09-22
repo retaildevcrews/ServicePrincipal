9de048bd0a0ec781fc9a3e2bc6139a060f28b17d
Joseph Fultz <jofultz@hotmail.com>
2020-09-22 13:08:46 -0500

    refactor WIP

--- src/Automation/CSE.Automation/Processors/DeltaProcessorBase.cs
+++ src/Automation/CSE.Automation/Processors/DeltaProcessorBase.cs
@@ -1,5 +1,6 @@
 ﻿using CSE.Automation.Interfaces;
 using CSE.Automation.Model;
+using CSE.Automation.Utilities;
 using Microsoft.Graph;
 using System;
 using System.Collections.Generic;
@@ -9,14 +10,30 @@ namespace CSE.Automation.Processors
 {
     public class DeltaProcessorBase : IDeltaProcessor
     {
-        public string ProcessorId { get; private set; }         
+        private IDAL _configDAL;
+        private Configuration _config;
+        private readonly string _uniqueId = string.Empty;
+        public string ProcessorId
+        {
+            get
+            {
+                return _uniqueId;
+            }
+        }
 
         public DeltaProcessorBase (IDAL configDAL)
         {
             if (configDAL is null)
                 throw new NullReferenceException("Null Configuration DAL passed to DeltaProcessor Constructor");
 
-            var config = configDAL.GetById<Configuration>(ProcessorId);
+            if (string.IsNullOrWhiteSpace(ProcessorId))
+                throw new NullReferenceException("Value not set for DeltaProcessor unique identifier");
+
+            _configDAL = configDAL;
+
+            // Need the config for startup, so accepting the blocking call in the constructor.
+            _config = configDAL.GetById<Configuration>(ProcessorId).Result;
+
         }
 
         public void ProcessDeltas()
---
+++ src/Automation/CSE.Automation/Processors/ServicePrincipalNotesProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSE.Automation.Interfaces;
+using CSE.Automation.Utilities;
+
+namespace CSE.Automation.Processors
+{
+    public class ServicePrincipalNotesProcessor:DeltaProcessorBase
+    {
+        private readonly string _uniqueId = "E7575910-2F85-4373-8284-403E7D530C55";
+        public ServicePrincipalNotesProcessor(IDAL configDAL):base(configDAL)
+        {
+            // TODO:
+            // 1. Create sample config and add to CosmosDB
+            // 2. Ensure config loads and parses properly
+            // 3. Write test to construct ServicrPrincipalNoateProcessor directly
+            // 4. Construct graph helper with config information
+            // 5. Refactor GraphHelper classes to use configuration information
+        }
+        public void ProcessDeltas()
+        {
+
+        }
+    }
+}
