75f6505a57f24aa957bccf30610fed28c8427af2
Gerardo Ortega <gortega@microsoft.com>
2020-09-23 09:51:20 -0500

    issue #81, #85 Provisioning key vault secrets for graph app registration and storage connection string

---
+++ infra/webapp/main-sp.tf
@@ -0,0 +1,19 @@
+# Create an application
+resource "azuread_application" "graphclient" {
+    name = "${var.PROJECT_NAME}-graph-${var.ENV}"
+}
+
+# Create a service principal
+resource "azuread_service_principal" "graphsp" {
+  application_id = azuread_application.graphclient.application_id
+
+}
+
+resource "azuread_application_password" "graphsppwd" {
+  depends_on = [azurerm_key_vault_secret.graphdappclientsecret]
+  application_object_id = azuread_application.graphclient.id
+  description           = "appGraphSecret"
+  value                 =  azurerm_key_vault_secret.graphdappclientsecret.value
+  end_date              = "2099-01-01T01:02:03Z"
+}
+
