3a003c3f97fcc7a6fc30eae5159e2d70a3bf79d0
Bart Robertson <bartr@microsoft.com>
2020-09-30 16:13:54 -0500

    Added codespaces support (#135)

---
+++ .devcontainer/post-install.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# copy vscode files
+mkdir -p .vscode
+cp .devcontainer/vscode-template/* .vscode
+
+# run dotnet restore
+dotnet restore src/Automation/CSE.Automation.sln
+
+# set auth type
+export PATH="$PATH:~/.dotnet/tools"
+export AUTH_TYPE=CLI
+
+# install WebV global tool
+dotnet tool install -g webvalidate
+
+# update .bashrc
+echo "" >> ~/.bashrc
+echo 'export PATH="$PATH:~/.dotnet/tools"' >> ~/.bashrc
+echo "export AUTH_TYPE=CLI" >> ~/.bashrc
