46e0def0baf5a8255bda558b0b2d9e3246e58d55
Sean Miller <semill@microsoft.com>
2020-10-02 10:22:08 -0700

    Adding unit tests for validator

---
+++ src/Automation/CSE.Automation.Tests/ModelValidationUnitTests/ModelValidationTests.cs
@@ -0,0 +1,46 @@
+﻿using NSubstitute;
+using Xunit;
+using CSE.Automation.Model;
+using FluentValidation;
+using System;
+
+namespace CSE.Automation.Tests.FunctionsUnitTests
+{
+    public class ModelValidationTests
+    {
+
+        [Fact]
+        public void ServicePrincipalModelValidate_ThrowsValidationExceptionIfInvalid()
+        {
+            var servicePrincipal = new ServicePrincipalModel
+            {
+                AppId = "fake app id",
+                AppDisplayName = "fake app display name",
+                DisplayName = "fake display name"
+            };
+
+            Action result = () => servicePrincipal.Validate(new CreateServicePrincipalValidator());
+
+            var exception = Assert.Throws<ValidationException>(result);
+
+            Assert.Contains("'Notes' must not be empty", exception.Message);
+        }
+
+        [Fact]
+        public void ServicePrinciapalModelValidate_DoesNotThrowExceptionIfValid()
+        {
+            var servicePrincipal = new ServicePrincipalModel
+            {
+                AppId = "fake app id",
+                AppDisplayName = "fake app display name",
+                DisplayName = "fake display name",
+                Id = "fake id",
+                Notes = "super fake note"
+            };
+
+            var exception = Record.Exception(() => servicePrincipal.Validate(new CreateServicePrincipalValidator()));
+
+            Assert.Null(exception);
+        }
+    }
+}
