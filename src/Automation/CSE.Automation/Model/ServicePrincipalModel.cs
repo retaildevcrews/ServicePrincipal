7850fa09628d9477a3126c8f87ce6482d3181e53
Sean Miller <semill@microsoft.com>
2020-10-02 09:46:27 -0700

    Adding validator to ServicePrincipalModel

--- src/Automation/CSE.Automation/Model/ServicePrincipalModel.cs
+++ src/Automation/CSE.Automation/Model/ServicePrincipalModel.cs
@@ -1,4 +1,7 @@
 ﻿
+using FluentValidation;
+using Microsoft.Extensions.Options;
+
 namespace CSE.Automation.Model
 {
     // Used name ServicePrincipalModel to disambiguate from Microsoft.Graph.ServicePrincipal
@@ -11,5 +14,29 @@ namespace CSE.Automation.Model
         public string DisplayName { get; set; }
 
         public string Notes { get; set; }
+
+        public void Validate(AbstractValidator<ServicePrincipalModel> validator)
+        {
+            if (validator == null)
+            {
+                throw new ValidationException("Validator must not be null.");
+            }
+
+            var validationResults = validator.Validate(this);
+
+            if (!validationResults.IsValid)
+            {
+                throw new ValidationException(validationResults.Errors);
+            }
+        }
+    }
+
+    // We can add more validation classes as needed for different types of validation for our models
+    public class CreateServicePrincipalValidator : AbstractValidator<ServicePrincipalModel>
+    {
+        public CreateServicePrincipalValidator()
+        {
+            RuleFor(x => x.Notes).NotNull();
+        }
     }
 }
