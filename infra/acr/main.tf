3a0da8d3dc4235c824d799cdd51fc4b1b6c01bf7
Alfredo Hernandez <52010147+alfredoihernandez@users.noreply.github.com>
2020-10-15 09:13:01 -0500

    Dockerize Function (#182)
    * changes after pushing docker to azure functions
    
    * add docker to terraform
    
    * fix docker being called by github action
    
    * fix terraform apply
    
    * typo
    
    * revert main.tf
    
    * revert main.tf again
    
    * remove non infrastruction constants
    
    Co-authored-by: alfredoihernandez <a14hdz@gmail.com.com>

--- infra/acr/main.tf
+++ infra/acr/main.tf
@@ -7,6 +7,11 @@ resource azurerm_container_registry svc-ppl-acr {
   sku                 = "Standard"
 }
 
+output "acr_uri" {
+  depends_on = [azurerm_container_registry.svc-ppl-acr]
+  value = azurerm_container_registry.svc-ppl-acr.login_server
+}
+
 resource null_resource acr-access {
   provisioner "local-exec" {
     command = "az role assignment create --scope ${azurerm_container_registry.svc-ppl-acr.id} --role acrpull --assignee ${var.ACR_SP_ID}"
