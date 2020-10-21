c9f0d4450938dfd8adbe0f9ce4c5cf0ce3fc48b6
gortegaMS <70150605+gortegaMS@users.noreply.github.com>
2020-10-21 18:46:05 -0500

    Create test utility to inject messages directly into the 'evaluate' queue and 'update' queue. #209 (#214)
    * Utility to add messages to Queues
    
    * Removed secrets

---
+++ spikes/AzQueueTestTool/AzQueueTestTool/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzQueueTestTool
+{
+    class Program
+    {
+        //Create test utility to inject messages directly into the 'evaluate' queue and 'update' queue.
+
+        static void Main(string[] args)
+        {
+            using (var queueSettings = new QueueSettings())
+            {
+
+                ShouldProceed(queueSettings);
+
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                StringBuilder sb = new StringBuilder();
+                Parallel.ForEach(queueSettings.QueueNamesList, (queueName) =>
+                {
+                    using (var queueManager = new QueueManager(queueName, queueSettings.MessageBase, queueSettings.StorageConnectionString))
+                    {
+                        queueManager.AddBaseMessages(queueSettings.MessageCount);
+                        sb.AppendLine(queueManager.StatusMessage);
+                    }
+                });
+
+                stopWatch.Stop();
+                Console.Clear();
+                Console.WriteLine(sb.ToString());
+                Console.WriteLine($"{Environment.NewLine}Process completed!, time elapsed - {stopWatch.Elapsed}");
+            }
+
+        }
+
+        private static void ShouldProceed(QueueSettings queueSettings)
+        {
+            string accountName = GetAccountName(queueSettings.StorageConnectionString);
+
+            Console.WriteLine($"Your target Storage Account is [{accountName}] and [{queueSettings.MessageCount}] messages will be pushed to each Queue [{queueSettings.QueueNames}]{Environment.NewLine}{Environment.NewLine}Enter 'Y' to continue?");
+
+            try
+            {
+                string toContinue = Console.ReadLine();
+
+                if (toContinue.Trim() != "Y")
+                {
+                    Console.WriteLine("Request was cancelled,  Goodbye!");
+                    Environment.Exit(0);
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Could not undestand your answer, please try again, Goodbye!");
+                Environment.Exit(-1);
+            }
+        }
+
+        private static string GetAccountName(string storageConnectionString)
+        {
+            List<string> storageConnectionStringPartsList = storageConnectionString.Split(';').ToList();
+
+            List<string> itemPartsList = storageConnectionStringPartsList.FirstOrDefault(x => x.StartsWith("AccountName")).Split('=').ToList();
+
+            return itemPartsList[1];
+
+        }
+    }
+}
