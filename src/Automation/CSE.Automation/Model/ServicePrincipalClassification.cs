cb49d053f2d5113f80580b4d2caeb49ebe87130a
jkeane <jikeane@microsoft.com>
2020-11-05 16:51:54 -0600

    Shell for classifier logic

---
+++ src/Automation/CSE.Automation/Model/ServicePrincipalClassification.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSE.Automation.Model
+{
+    internal class ServicePrincipalClassification
+    {
+        public Guid OwningTenant { get; set; }
+        public string Type { get; set; }
+        public ObjectClassification Classification { get; set; }
+        public string Category { get; set; }
+        public string OwningTenantDomain { get; set; }
+        public bool HasOwner { get; set; }
+    }
+}
