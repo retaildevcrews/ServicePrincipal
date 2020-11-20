5a533c5593384e79f81da7d6971fd75185cea32c
Alfredo Hernandez <52010147+alfredoihernandez@users.noreply.github.com>
2020-11-20 15:30:59 -0600

    Service Principal Classification Spike (#375)
    * first attempt at reading mapping configuations
    
    * add catch all for external/third party
    
    * jk file
    
    * Integrate Jims work
    
    * add oobe mapping
    
    * WIP
    
    * write output to file
    
    * add arg validation, support various output types
    
    * Write summaries to console
    
    * formatting
    
    * remove WIP script
    
    * parameter changes
    
    * print summary on passthru
    
    * lint fix
    
    * Add Documentation
    
    * add purpose
    
    * EOF Lint Fix
    
    Co-authored-by: alfredoihernandez <a14hdz@gmail.com.com>
    Co-authored-by: jkeane <jikeane@microsoft.com>

---
+++ spikes/ServicePrincipalClassification/Classification.ps1
@@ -0,0 +1,132 @@
+[CmdletBinding()]
+param (
+  [string]$OutputFile = $null,
+
+  [ValidateSet("csv", "tsv", "json")]
+  [string]$OutputType = "csv", 
+
+  [switch]$PassThru
+)
+
+function ValidateArguments
+{
+  if ([string]::IsNullOrWhiteSpace($OutputFile) -and -not $PassThru) {
+    Write-Host "File Path Not Set, Only Printing Summary"
+  }
+}
+
+$ClassificationMapping = Get-Content './resources/classification_mapping.json' | ConvertFrom-Json
+
+$CategoryOobeList = Get-Content './resources/category_oobe_list.json' | ConvertFrom-Json
+
+function ClassifyMicrosoft
+{
+  [CmdletBinding()]
+  param (
+    $group
+  )
+  $group.Group |
+    ForEach-Object {
+      $_.Tags += "Microsoft"
+      if ($CategoryOobeList -contains $_.AppId){
+        $_.Tags += "OOBE"
+      }
+      else {
+        $_.Tags += "Addition"
+      }
+    }
+}
+
+function ClassifyTenant
+{
+  [CmdletBinding()]
+  param (
+    $group
+  )
+  $group.Group |
+    ForEach-Object {
+      $_.Tags += "Tenant"
+      $_.Tags += $_.ServicePrincipalType
+    }
+}
+
+function ClassifyThirdParty
+{
+  [CmdletBinding()]
+  param (
+    $group
+  )
+  $group.Group |
+    ForEach-Object {
+      $_.Tags += "ThirdParty"
+      $_.Tags += $_.ServicePrincipalType
+    }
+}
+
+function ClassifyGroup
+{
+  [CmdletBinding()]
+  param (
+    [string]$groupName
+  )
+
+  
+  $classification = $ClassificationMapping | ForEach-Object {
+    if ($_.MatchValues -contains $groupName)
+    {
+      return $_.Classification
+    }
+  }
+
+  if ([string]::IsNullOrWhiteSpace($classification)) {
+    return "ThirdParty"
+  }
+  else {
+    return $classification
+  }
+}
+
+ValidateArguments
+
+connect-graph -Scopes "Directory.read.all" | Out-Null
+$spList = Get-MgServicePrincipal -All
+
+$groups = $spList | Group-Object -Property ServicePrincipalType,AppOwnerOrganizationId | Sort-Object Count -D
+
+Write-Host "`nSummary of Service Principals Retrieved:"
+$groups | Select-Object -Property Count, @{N='AppOwnerOrganizationId';E={$_.Group[0].AppOwnerOrganizationId}}, @{N='ServicePrincipalType';E={$_.Group[0].ServicePrincipalType}} | Out-String | Write-Host
+
+$groups | 
+  ForEach-Object {
+    $groupClass = ClassifyGroup $_.Name
+    if ($groupClass -eq 'Microsoft') {
+      ClassifyMicrosoft $_
+    }
+    elseif ($groupClass -eq 'Tenant') {
+      ClassifyTenant $_
+    }
+    else {
+      ClassifyThirdParty $_
+    }
+  }
+
+  $results = $spList |
+      Select-Object -Property @{N='Classification';E={$_.Tags[-2]}}, @{N='Category';E={$_.Tags[-1]}}, Id, AppOwnerOrganizationId, AppId, DisplayName, @{N='ServicePrincipalNames';E={$_.ServicePrincipalNames -join ", "}}, ServicePrincipalType
+
+  Write-Host "Summary of Results:"
+      $results | Group-Object -Property Classification, Category | Sort-Object Count -D | Select-Object -Property Count, @{N='Classification';E={$_.Group[0].Classification}}, @{N='Category';E={$_.Group[0].Category}} | Out-String | Write-Host
+
+  if (-not ([string]::IsNullOrWhiteSpace($OutputFile))) {
+
+    if ($OutputType -eq "csv") {
+      $results | ConvertTo-Csv | Out-File -FilePath $OutputFile
+    } elseif ($OutputType -eq "tsv") {
+      $results | ConvertTo-Csv -Delimiter "`t" | Out-File -FilePath $OutputFile
+    } elseif ($OutputType -eq "json") {
+      $results | ConvertTo-Json | Out-File -FilePath $OutputFile
+    } 
+
+  }
+  else {
+    return $results
+  }
