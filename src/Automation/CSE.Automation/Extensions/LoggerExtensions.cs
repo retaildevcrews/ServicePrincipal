b5c2754075a0f6293a2f949f8fc4d77588c3d087
jkeane <jikeane@microsoft.com>
2021-02-19 15:02:04 -0600

    added file headers

--- src/Automation/CSE.Automation/Extensions/LoggerExtensions.cs
+++ src/Automation/CSE.Automation/Extensions/LoggerExtensions.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using System;
 using System.Diagnostics;
-using System.Text;
 using Microsoft.Extensions.Logging;
 
 namespace CSE.Automation.Extensions
