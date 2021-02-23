3611e0c70145a723e4087105d314dc84f37cdce6
jkeane <jikeane@microsoft.com>
2021-02-23 16:50:11 -0600

    added shell test data generator for Update

---
+++ src/Automation/CSE.Automation.Tests/TestDataGenerators/ServicePrincipalDiscoverTestData.cs
@@ -0,0 +1,19 @@
+﻿using CSE.Automation.Model;
+using Microsoft.Graph;
+
+namespace CSE.Automation.Tests.TestDataGenerators
+{
+    public class ServicePrincipalDiscoverTestData
+    {
+        internal RequestDiscoveryCommand Target { get; set; }
+        internal TrackingModel[] InitialObjectServiceData { get; set; }
+        internal TrackingModel[] ExpectedObjectServiceData { get; set; }
+        internal AuditCode[] ExpectedAuditCodes { get; set; }
+        internal ServicePrincipal[] InitialServicePrincipals { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Target.DiscoveryMode}, {Target.Source}";
+        }
+    }
+}
